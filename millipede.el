9a08a4b06ce6ef070ca5930c0663a998e60dd377
Mathieu Fournier <mathieu.fournier@rentabiliweb.com>
2015-08-07 10:37:12 +0200

    Initial commit

---
+++ millipede.el
@@ -0,0 +1,35 @@
+#!/usr/bin/emacs --script
+
+(setq head "    ╚⊙ ⊙╝\n")
+(setq body "╚═(███)═╝\n")
+(setq padding (vector "  " " " "" " " "  " "   " "    " "   "))
+
+(defun millipede (size)
+    "Print each element of LIST on a line of its own."
+    (setq i 0)
+    (princ head)
+    (while (< i size)
+        (princ (elt padding (% i 8)))
+        (princ body)
+        (setq i (+ i 1))
+        )
+)
+
+(defun main ()
+    "Main function"
+    (if (elt argv 0)
+        (setq size (string-to-number (elt argv 0)))
+        (if '(integerp size)
+                (setq size 21)
+            )
+            (setq size 22)
+        )
+
+    (when (<= size 2)
+            (setq size 20)
+        )
+
+    (millipede size)
+)
+
+(main)
